7e073958e6a92537dd1bb5ae8f9d26a84e273a71
sandrabosk <sandraboskovic@hotmail.com>
2018-02-22 10:41:47 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -2,24 +2,113 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+// Verb: GET, Route: "/characters"
+// It receives NO parameters
+// It returns the full characters list
+// It returns JSON
   getFullList () {
+    axios.get(`${this.BASE_URL}/characters`)
+    .then((response)=>{
+      // console.log("response is: ", response.data);
+      // document.getElementsByClassName("characters-container")[0].innerHTML = "";
+      $(".characters-container").empty();
+      response.data.forEach(oneRes => {
+        const myHtml = 
+        $(
+          `<div class="character-info">
+            <div class="id">Id: ${oneRes.id}</div>
+            <div class="name">Name: ${oneRes.name}</div>
+            <div class="occupation">Occupation: ${oneRes.occupation}</div>
+            <div class="debt">Debt: ${oneRes.debt}</div>
+            <div class="weapon">Weapon: ${oneRes.weapon}</div>
+          </div>`
+        );
+        $(".characters-container").append(myHtml);
+
+      //   const myHtml = 
+      //   `<div class="character-info">
+      //       <div class="id">Id: ${oneRes.id}</div>
+      //       <div class="name">Name: ${oneRes.name}</div>
+      //       <div class="occupation">Occupation: ${oneRes.occupation}</div>
+      //       <div class="debt">Debt: ${oneRes.debt}</div>
+      //       <div class="weapon">Weapon: ${oneRes.weapon}</div>
+      //   </div>`;
+      // document.getElementsByClassName("characters-container")[0].innerHTML += myHtml;
+
+      });
+
+
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
 
   }
-
-  getOneRegister () {
-
+// Verb: GET, Route: "/characters/:id"
+// It receives the character ID as a parameter (route)
+// It returns the character with the indicated id
+// It returns JSON
+  getOneRegister (id) {
+    axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then(response => {
+        // console.log("oneRegister response is:", response);
+        $(".characters-container").empty();
+        const singleCharacter = $(`<div class="character-info">
+            <div class="id">Id: ${response.data.id}</div>
+            <div class="name">Name: ${response.data.name}</div>
+            <div class="occupation">Occupation: ${response.data.occupation}</div>
+            <div class="debt">Debt: ${response.data.debt}</div>
+            <div class="weapon">Weapon: ${response.data.weapon}</div>
+          </div>`);
+        $(".characters-container").append(singleCharacter);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  createOneRegister () {
-
+// Verb: POST, Route: "/characters"
+// It receives an object as a parameter, with the following format: { name: string, occupation: string, debt: boolean, weapon: string }
+// It returns the created character if there are no errors
+// It returns the wrong fields if there is some error
+// It returns JSON
+  createOneRegister (data) {
+    axios
+      .post(this.BASE_URL + "/characters/", data)
+      .then((response) => {
+        // console.log("created: ", response)
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  updateOneRegister () {
-
+// It receives the character id as a parameter (route)
+// It receives an object as a parameter, with the following format: { name: string, occupation: string, debt: boolean, weapon: string }
+// All the fields are optionals
+// It returns the updated character if there are no errors
+// It returns "Character not found" if there is no character with the indicated id
+// It returns JSON / text
+  updateOneRegister (id, data) {
+    axios.patch(this.BASE_URL + "/characters/" + id, data)
+      .then(response => {
+        console.log("updated: ", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+// It receives the character id as a parameter (route)
+// It returns "Character has been successfully deleted" if there are no errors
+// It returns "Character not found" if there is no character with the indicated id
+// It returns text
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + "/characters/" + id)
+      .then((response) => {
+        console.log("deleted: ", response);
+      })
+      .catch();
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,77 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
-  }
+    const id = document.getElementsByName("character-id")[0].value;
+    console.log("the id is:", id);
+    charactersAPI.getOneRegister(id);
+  };
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const id = document.getElementsByName("character-id-delete")[0].value;
+    console.log("deleted char has id: ", id);
+    charactersAPI.deleteOneRegister(id);
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
+    event.preventDefault(); 
+    const id = document.getElementsByName("chr-id")[0].value;
+    // console.log("id", id);
+    
+const name = document.getElementsByName("name")[1].value;
+const occupation = document.getElementsByName("occupation")[1].value;
+const debt = document.getElementsByName("debt")[1].checked;
+const weapon = document.getElementsByName("weapon")[1].value;
+
+    const updatedCharacter = {};
+
+    if (name !== "") {
+      updatedCharacter.name = document.getElementsByName("name")[1].value;
+    } 
+    if (occupation !== "") {
+      updatedCharacter.occupation = document.getElementsByName("occupation")[1].value;
+    } 
+    if (debt === "true") {
+      updatedCharacter.debt = document.getElementsByName("debt")[1].checked;
+    } 
+    if (weapon !== "") {
+      updatedCharacter.weapon = document.getElementsByName("weapon")[1].value;
+    }
+
+    charactersAPI.updateOneRegister(id, updatedCharacter);
+   
+  };
   
   document.getElementById('new-character-form').onsubmit = function(){
+    event.preventDefault(); 
+
+    const name = document.getElementsByName("name")[0].value;
+    const occupation = document.getElementsByName("occupation")[0].value;
+    const debt = document.getElementsByName("debt")[0].checked;
+    const weapon = document.getElementsByName("weapon")[0].value;
+
+    if (name === ""){
+      console.log("Input the name please");
+    } else if (occupation === ""){
+      console.log("Input the occupation please");
+    } else if (debt === "") {
+      console.log("Input the debt please");
+    } else if (weapon === "") {
+      console.log("Input the weapon please");
+    } else {
+        const myCharacter = {
+          name : document.getElementsByName("name")[0].value,
+          occupation : document.getElementsByName("occupation")[0].value,
+          debt : document.getElementsByName("debt")[0].checked,
+          weapon:document.getElementsByName("weapon")[0].value
+        };  
+        console.log("MyChar: ", myCharacter)
+        charactersAPI.createOneRegister(myCharacter);
+    }
+
                 
   }
 })
